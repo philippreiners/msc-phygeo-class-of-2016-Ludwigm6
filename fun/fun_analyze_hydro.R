e16f0c197ac9418621d8e0a3fee78f71b98f7593
Ludwigm6 <marvin.ludwig93@gmail.com>
2016-12-13 18:09:15 +0100

    analyze_hydro_removeSPI

--- fun/fun_analyze_hydro.R
+++ fun/fun_analyze_hydro.R
@@ -1,6 +1,6 @@
 # fun: Hydrological properties of a DEM
 
-analyze_hydro <- function(DEM, workdir, sink_min_slope, channels_threshold){
+analyze_hydro <- function(DEM, workdir, sink_min_slope = "0.1", channels_threshold){
  
   library(gdalUtils)
   library(rgdal)
@@ -35,27 +35,27 @@ analyze_hydro <- function(DEM, workdir, sink_min_slope, channels_threshold){
   
   # 3. Stream Power Index
   # 3.1 Slope, Aspect, Curvature
-  system(paste0("saga_cmd ta_morphometry 0 ",
-                "-ELEVATION=DEM_no_sinks.sdat ",
-                "-SLOPE=slope.sdat ",
-                "-METHOD=6 -UNIT_SLOPE=0 -UNIT_ASPECT=0"))
+  #system(paste0("saga_cmd ta_morphometry 0 ",
+  #              "-ELEVATION=DEM_no_sinks.sdat ",
+  #              "-SLOPE=slope.sdat ",
+  #              "-METHOD=6 -UNIT_SLOPE=0 -UNIT_ASPECT=0"))
   
   # 3.2 Stream Power Index
-  system(paste0("saga_cmd ta_hydrology 21 ",
-                "-SLOPE=slope.sdat ",
-                "-AREA=watershed.sgrd ",
-                "-SPI=stream_power_index.sdat ",
-                "-CONV=0"))
+  #system(paste0("saga_cmd ta_hydrology 21 ",
+  #              "-SLOPE=slope.sdat ",
+  #              "-AREA=watershed.sgrd ",
+  #              "-SPI=stream_power_index.sdat ",
+  #              "-CONV=0"))
   
   
   # 3.3 Convert to tif
-  gdalwarp(srcfile = paste0(workdir, "stream_power_index.sdat"), dstfile = paste0(workdir, "stream_power_index.tif"), overwrite = TRUE, of = 'GTiff')
+  #gdalwarp(srcfile = paste0(workdir, "stream_power_index.sdat"), dstfile = paste0(workdir, "stream_power_index.tif"), overwrite = TRUE, of = 'GTiff')
   
   
   
   output <- list(channels = readOGR(paste0(workdir, "channels.shp")),
                  watershed = readOGR(paste0(workdir, "watershed_basins.shp")),
-                 spi = raster(paste0(workdir, "stream_power_index.tif")),
+                 #spi = raster(paste0(workdir, "stream_power_index.tif")),
                  filepath = workdir)
   
   return(output)
