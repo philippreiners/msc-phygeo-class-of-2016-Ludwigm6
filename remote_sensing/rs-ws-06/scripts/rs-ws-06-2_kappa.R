a1866b58254a01d07ce69e719b3e1f824294782e
Ludwigm6 <marvin.ludwig93@gmail.com>
2016-12-21 12:20:22 +0100

    rs-ws-06-2_inArbeit

---
+++ remote_sensing/rs-ws-06/scripts/rs-ws-06-2_kappa.R
@@ -0,0 +1,39 @@
+# rs-ws-06-2 Kappa
+
+source("d:/university/msc-phygeo-class-of-2016-Ludwigm6/fun/init.R")
+
+path <- fun_init("rs", "06")
+path$samples <- paste0(path$rs$run, "samples/")
+source(path$fun$fun_compKappa.R)
+
+
+matrix_files <- list.files(path$samples, pattern = "*.csv", full.names = TRUE)
+
+kappa <- lapply(seq(length(matrix_files)), function(x){
+  
+  # load matrix and extract rows and cols
+  m <- read.csv(matrix_files[[x]], skip = 0, header = FALSE)
+  rows <- unlist(m[1,])
+  cols <- unlist(m[2,])
+  
+  # load matrix again; skip the row index and with cols as header
+  m <- read.csv(matrix_files[[x]], skip = 1, header = TRUE)
+  
+  
+  # make the matrix a square
+  template <- matrix(nrow = 40, ncol = 40, data = 0)
+  for(i in seq(40)){
+    for(j in seq(40)){
+      if(i %in% rows & j %in% cols){
+        template[i,j] <- m[which(rows == i), which(cols == j)]
+      }
+    }
+  }
+  
+  return(compKappa(ctable = template))
+
+})
+
+
+
+
--- remote_sensing/rs-ws-06/scripts/rs-ws-06-2_separate_data.R
+++ remote_sensing/rs-ws-06/scripts/rs-ws-06-2_separate_data.R
@@ -1,6 +1,4 @@
-# rs-ws-06-2 accuarcy assessment
-#install.packages("caret")
-
+# rs-ws-06-2 train and test separation
 
 source("d:/university/msc-phygeo-class-of-2016-Ludwigm6/fun/init.R")
 path <- fun_init("rs", "06")
@@ -10,20 +8,25 @@ path$samples <- paste0(path$rs$run, "samples/")
 library(caret)
 library(rgdal)
 library(raster)
-source(path$fun$fun_compKappa.R)
 
+# load training sites 
 train_sites <- readOGR(paste0(path$rs$run, "training_sites.shp"))
+
+# write each polygon in a list
 separate <- lapply(seq(length(train_sites)), function(i) train_sites[i,])
 
+# get 80 % of the index numbers (6 times)
 sites_sample <- createDataPartition(train_sites@data$ID, p = 0.80, list = FALSE, times = 6)
 
 
-
+# samples is a list with 6 entries. each entry contains two spatialpolygondataframes: train and test
 samples <- lapply(seq(ncol(sites_sample)), function(x){
   
+  # get 80% of the polygons based on the numbers in sites_sample
   train <- lapply(sites_sample[,x], function(i) rbind(separate[[i]]))
   train <- do.call("rbind", train)
   
+  # get the remaining 20%
   test <- lapply(seq(length(train_sites))[-sites_sample[,x]], function(j) rbind(separate[[j]]))
   test <- do.call("rbind", test)  
   
@@ -31,66 +34,12 @@ samples <- lapply(seq(ncol(sites_sample)), function(x){
   
 })
 
-
+# create output folder
 dir.create(paste0(path$samples))
 
+# save each list entry as shapefile 
 for(k in seq(6)){
   writeOGR(samples[[k]]$train, dsn = paste0(path$sample, "train_", as.character(k), ".shp"), driver = "ESRI Shapefile", layer = "train")
   writeOGR(samples[[k]]$test, dsn = paste0(path$sample, "test_", as.character(k), ".shp"), driver = "ESRI Shapefile", layer = "test")
 }
 
-
-# load matrix and extract rows and cols
-matrix_1 <- read.csv(paste0(path$samples, "matrix_1.csv"), skip = 0, header = FALSE)
-rows <- unlist(matrix_1[1,])
-cols <- unlist(matrix_1[2,])
-
-# load matrix again; skip the row index and with cols as header
-matrix_1 <- read.csv(paste0(path$samples, "matrix_1.csv"), skip = 1, header = TRUE)
-
-
-# make the matrix a square
-template <- matrix(nrow = 40, ncol = 40, data = 0)
-for(i in seq(40)){
-  for(j in seq(40)){
-    if(i %in% rows & j %in% cols){
-      template[i,j] <- matrix_1[which(rows == i), which(cols == j)]
-    }
-  }
-}
-
-
-kappa_1 <- compKappa(ctable = template)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# repair confMatrix
-
-
-
-
-
-matrix_1[,][is.na(matrix_1)] <- 0
-
-
-
-template <- matrix(nrow = 3, ncol = 3, data = c(1:9))
-template[2,2] <- 7
-template
-                       
