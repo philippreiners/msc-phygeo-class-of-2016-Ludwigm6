cb5c164ab59dc35b7d1d039d7dcc3aa2fed5f134
Ludwigm6 <marvin.ludwig93@gmail.com>
2017-01-24 13:16:54 +0100

    gi-ws-08-2_final

--- gis/gi-ws-08/scripts/gi-ws-08-2_biomass.R
+++ gis/gi-ws-08/scripts/gi-ws-08-2_biomass.R
@@ -7,21 +7,24 @@ path <-  fun_init("gi", "08")
 library(raster)
 library(rgdal)
 
-# load DEM and DSM
+# load dem, dtm, ground returns and above ground returns
+# list.files(path$gi$input)
 dem <- raster(paste0(path$gi$input, "lidar_dem_01m.tif"))
 dsm <- raster(paste0(path$gi$input, "lidar_dsm_01m.tif"))
+pcag <- raster(paste0(path$gi$input, "lidar_pcag_01m.tif"))
+pcgr <- raster(paste0(path$gi$input, "lidar_pcgr_01m.tif"))
 
-trees <- readOGR(paste0(path$gi$run, "trees/trees.shp"))
-plot(trees)
+# forest density can be determinated as the ratio of above ground returns to all returns:
+dens <- pcag/(pcgr+pcag)
 
-# Stem_biomass = -13.595 + 8.446*mean height + 20.378 *cover
-# branch = -2.447 + 1.367*mean height + 3.3 *cover
-
-
-
-
+# models for biomass from table 2 in He et al. 2013
+stem_biomass <- -13.595 + 8.446*mean(dsm-dem) + 20.378*dens
+branch_biomass <- -2.447 + 1.367*mean(dsm-dem) + 3.3 *dens
+wood <- stem_biomass + branch_biomass
 
 
+plot(wood)
+summary(wood)
 
 
 
