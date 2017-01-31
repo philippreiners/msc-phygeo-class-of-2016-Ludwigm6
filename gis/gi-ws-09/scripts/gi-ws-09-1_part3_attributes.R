0a4335ee29089992180afe63b33ae5924a215dea
Ludwigm6 <marvin.ludwig93@gmail.com>
2017-01-31 14:25:53 +0100

    gi-ws-09-1_final

--- gis/gi-ws-09/scripts/gi-ws-09-1_part3_attributes.R
+++ gis/gi-ws-09/scripts/gi-ws-09-1_part3_attributes.R
@@ -23,17 +23,24 @@ linkGRASS7(x = input$lidar_dem_01m.tif, searchPath = "C:\\GIS\\QGIS")
 
 # clean up attribute df
 names(trees@data) <- c("id_tree", "nodes")
+trees@data$id_crown <- NA
 
-# match trees with crowns
-# v.what
+crowns@data$cat <- NULL
+crowns@data$NAME <- NULL
+crowns@data$ID <- seq(length(crowns))
+names(crowns@data) <- c("id_crown", "id_tree")
+crowns@data$id_tree <- NA
+crowns@data$area <- NA
 
-
-
-
+writeOGR(trees, dsn = paste0(path$gi$run, "trees.shp"), driver = "ESRI Shapefile", layer = "trees" ,overwrite = TRUE)
+writeOGR(crowns, dsn = paste0(path$gi$run, "crowns.shp"), driver = "ESRI Shapefile", layer = "crowns", overwrite = TRUE)
 
 # # # tree height and type # # #
+# tree height
 chm <- input$lidar_dsm_01m.tif - input$lidar_dem_01m.tif
-writeRaster(chm, filename = paste0(path$gi$run, "tree_height.tif"))
+writeRaster(chm, filename = paste0(path$gi$run, "tree_height.tif"), overwrite = TRUE)
+
+# import all needed maps in GRASS
 execGRASS("r.import", flags = c("o", "overwrite"),
           parameters = list(input = paste0(path$gi$run, "tree_height.tif"),
                             output = "tree_height"))
@@ -43,7 +50,12 @@ execGRASS("r.import", flags = c("o", "overwrite"),
 execGRASS("v.import", flags = c("o", "overwrite"),
           parameters = list(input = paste0(path$gi$run, "trees.shp"),
                             output = "trees"))
+execGRASS("v.import", flags = c("o", "overwrite"),
+          parameters = list(input = paste0(path$gi$run, "crowns.shp"),
+                            output = "crowns"))
 
+
+# # # querying: get values from raster at points # # #
 execGRASS("r.what", flags = c("overwrite", "n"),
           parameters = list(map = c("tree_height","lcc"),
                             points = "trees",
@@ -51,15 +63,33 @@ execGRASS("r.what", flags = c("overwrite", "n"),
                             separator = "comma"))
 
 heights <- read.csv(paste0(path$gi$run, "heights.csv"), header = TRUE, sep = ",", dec = ".")
+# legend for lcc
 type <- read.csv(paste0(path$gi$run, "tree_type.csv"), header = TRUE, sep = ",")
 
+# attach the right type, age and height to attribute table
 heights$type <- type$type[match(heights$lcc, type$id)]
 heights$age <- type$age[match(heights$lcc, type$id)]
-
 
 trees@data$height <- heights$tree_height
 trees@data$tree_type <- heights$type
 trees@data$age <- heights$age
+
+head(trees@data)
+
+# # # match crowns with trees # # #
+# testing second query possibility which directly updates the column
+execGRASS("v.what.vect", parameters = list(map = "trees",
+                                           column = "id_crown",
+                                           query_map = "crowns",
+                                           query_column = "id_crown"))
+# only the GRASS vector is updated; we need to update our SPDF
+temp_trees <- readVECT(vname = "trees")
+trees@data$id_crown <- temp_trees@data$id_crown
+crowns@data$id_tree <- trees@data$id_tree[match(crowns@data$id_crown, trees@data$id_crown)]
+
+# save both shapefiles
+writeOGR(trees, dsn = paste0(path$gi$output, "trees.shp"), driver = "ESRI Shapefile", layer = "trees" ,overwrite = TRUE)
+writeOGR(crowns, dsn = paste0(path$gi$output, "crowns.shp"), driver = "ESRI Shapefile", layer = "crowns", overwrite = TRUE)
 
 
 # tree density heatmap
